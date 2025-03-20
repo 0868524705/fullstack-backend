29c9f89cfe469783941cf47877cfc11d8ac80741
phuongnguyen <phuongnguyenv27@example.com>
2025-03-20 12:49:30 +0700

    static files

--- src/app.js
+++ src/app.js
@@ -8,7 +8,8 @@ const hostname = process.env.HOSTNAME; // hostname
 // config template engine
 app.set('views', path.join(__dirname,'views/'))
 app.set('view engine', 'ejs')
-
+// config static files
+app.use(express.static(path.join(__dirname, "public")));
 // routes
 app.get('/', (req, res) => {
   // res.send('Hello World!')
