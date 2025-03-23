b6cb7b9acee8bae60e92864817a51b0381907fea
phuongnguyen <phuongnguyenv27@example.com>
2025-03-23 10:05:43 +0700

    connectToDatabase

--- src/app.js
+++ src/app.js
@@ -5,6 +5,30 @@ const app = express(); // app express
 const route = require("./routes/web"); // routes
 const port = process.env.PORT || 3333; // port
 const hostname = process.env.HOSTNAME; // hostname
+// database
+const mysql = require('mysql2');
+
+// Create the connection to database
+
+    const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  });
+
+
+
+// A simple SELECT query
+    connection.query(
+      'SELECT * from Users u',
+      function(err, results, fields) {
+        console.log(">>>results= ", results); // results contains rows returned by server
+        console.log(">>>fields= ", fields); // fields contains extra meta data about results, if available
+      }
+    );
+
 
 // config template engine
 configViewEngine(app);
