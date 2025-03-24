b7fdf587da015c41ee7e8c8ab4c3b17168da96ea
phuongnguyen <phuongnguyenv27@example.com>
2025-03-24 14:11:17 +0700

    create pools

--- src/app.js
+++ src/app.js
@@ -7,21 +7,6 @@ const port = process.env.PORT || 3333; // port
 const hostname = process.env.HOSTNAME; // hostname
 const connection = require('./config/database');
 
-// Create the connection to database
-
-
-
-
-
-// A simple SELECT query
-    connection.query(
-      'SELECT * from Users u',
-      function(err, results, fields) {
-        console.log(">>>results= ", results); // results contains rows returned by server
-        console.log(">>>fields= ", fields); // fields contains extra meta data about results, if available
-      }
-    );
-
 
 // config template engine
 configViewEngine(app);
